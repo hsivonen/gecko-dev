a00aeda637e9189feb24417bf100bfb4b697057c
PanderMusubi <pander@users.sourceforge.net>
2020-06-07 13:00:57 +0200

    improved unit testing, ongoing

--- extensions/spellcheck/nuspell/tests/unit/test_nuspell.js
+++ extensions/spellcheck/nuspell/tests/unit/test_nuspell.js
@@ -1,87 +1,113 @@
-/* Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/
+/* Copyright 2020 Sander van Geloven, Dimitrij Mijoski
+ *
+ * This file is part of Nuspell.
+ *
+ * Nuspell is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nuspell is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Nuspell.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 const tests = [
-  ["affixes", "iso-8859-1"],
-  ["condition", "iso-8859-1"],
-  ["condition-utf", "UTF-8"],
+//  ["1463589", "iso-8859-1"],
+  ["1463589_utf", "UTF-8"],
+  ["1592880", "iso-8859-1"],
+//  ["1695964", "iso-8859-1"],
+  ["1706659", "iso-8859-1"],
+  ["1975530", "UTF-8"],
+
+//  ["2970240", "iso-8859-1"],
+//  ["2970242", "iso-8859-1"],
+//  ["2999225", "iso-8859-1"],
+
+//  ["IJ", "iso-8859-1"],
+
+//  ["affixes", "iso-8859-1"],
+//  ["alias", "iso-8859-1"],
+//  ["alias2", "iso-8859-1"],
+//  ["alias3", "iso-8859-1"],
+//  ["allcaps", "iso-8859-1"],
+//  ["allcaps2", "iso-8859-1"],
+//  ["allcaps3", "iso-8859-1"],
+  ["allcaps_utf", "UTF-8"],
+  ["arabic", "UTF-8"],
+
   ["base", "iso-8859-1"],
-  ["base-utf", "UTF-8"],
-  ["allcaps", "iso-8859-1"],
-  ["allcaps-utf", "UTF-8"],
-  ["allcaps2", "iso-8859-1"],
-  ["allcaps3", "iso-8859-1"],
-  ["keepcase", "iso-8859-1"],
-  ["i58202", "iso-8859-1"],
-  ["map", "iso-8859-1"],
-  ["rep", "iso-8859-1"],
-  ["sug", "iso-8859-1"],
-  ["sugutf", "UTF-8"],
-  ["phone", "iso-8859-1"],
-  ["flag", "iso-8859-1"],
-  ["flaglong", "iso-8859-1"],
-  ["flagnum", "iso-8859-1"],
-  ["flagutf8", "UTF-8"],
-  ["slash", "iso-8859-1"],
-  ["forbiddenword", "iso-8859-1"],
-  ["nosuggest", "iso-8859-1"],
-  ["alias", "iso-8859-1"],
-  ["alias2", "iso-8859-1"],
-  ["alias3", "iso-8859-1"],
-  ["breakdefault", "iso-8859-1"],
+  ["base_utf", "UTF-8"],
   ["break", "UTF-8"],
-  ["needaffix", "iso-8859-1"],
-  ["needaffix2", "iso-8859-1"],
-  ["needaffix3", "iso-8859-1"],
-  ["needaffix4", "iso-8859-1"],
-  ["needaffix5", "iso-8859-1"],
-  ["circumfix", "iso-8859-1"],
-  ["fogemorpheme", "iso-8859-1"],
-  ["onlyincompound", "iso-8859-1"],
-  ["complexprefixes", "iso-8859-1"],
-  ["complexprefixes2", "iso-8859-1"],
-  ["complexprefixesutf", "UTF-8"],
-  ["conditionalprefix", "iso-8859-1"],
-  ["zeroaffix", "iso-8859-1"],
-  ["utf8", "UTF-8"],
-  ["utf8-bom", "UTF-8", { 1: "todo" }],
-  ["utf8-bom2", "UTF-8", { 1: "todo" }],
-  ["utf8-nonbmp", "UTF-8", { 1: "todo", 2: "todo", 3: "todo", 4: "todo" }],
-  ["compoundflag", "iso-8859-1"],
-  ["compoundrule", "iso-8859-1"],
-  ["compoundrule2", "iso-8859-1"],
-  ["compoundrule3", "iso-8859-1"],
-  ["compoundrule4", "iso-8859-1"],
-  ["compoundrule5", "UTF-8"],
-  ["compoundrule6", "iso-8859-1"],
-  ["compoundrule7", "iso-8859-1"],
-  ["compoundrule8", "iso-8859-1"],
-  ["compoundaffix", "iso-8859-1"],
-  ["compoundaffix2", "iso-8859-1"],
-  ["compoundaffix3", "iso-8859-1"],
-  ["checkcompounddup", "iso-8859-1"],
-  ["checkcompoundtriple", "iso-8859-1"],
-  ["simplifiedtriple", "iso-8859-1"],
-  ["checkcompoundrep", "iso-8859-1"],
-  ["checkcompoundcase2", "iso-8859-1"],
+  ["breakdefault", "UTF-8"],
+  ["breakoff", "UTF-8"],
+
+//  ["checkcompoundcase", "iso-8859-1"],
+//  ["checkcompoundcase2", "iso-8859-1"],
   ["checkcompoundcaseutf", "UTF-8"],
-  ["checkcompoundpattern", "iso-8859-1"],
-  ["checkcompoundpattern2", "iso-8859-1"],
-  ["checkcompoundpattern3", "iso-8859-1"],
-  ["checkcompoundpattern4", "iso-8859-1"],
-  ["utfcompound", "UTF-8"],
-  ["checksharps", "iso-8859-1"],
+//  ["checkcompounddup", "iso-8859-1"],
+//  ["checkcompoundpattern", "iso-8859-1"],
+//  ["checkcompoundpattern2", "iso-8859-1"],
+//  ["checkcompoundpattern3", "iso-8859-1"],
+//  ["checkcompoundpattern4", "iso-8859-1"],
+//  ["checkcompoundrep", "iso-8859-1"],
+//  ["checkcompoundtriple", "iso-8859-1"],
+//  ["checksharps", "iso-8859-1"],
   ["checksharpsutf", "UTF-8"],
-  ["germancompounding", "iso-8859-1"],
-  ["germancompoundingold", "iso-8859-1"],
+//  ["circumfix", "iso-8859-1"],
+//  ["colons_in_words", "iso-8859-1"], Suggestion test only
+//  ["complexprefixes", "iso-8859-1"],
+//  ["complexprefixes2", "iso-8859-1"],
+  ["complexprefixesutf", "UTF-8"],
+//  ["compoundaffix", "iso-8859-1"],
+//  ["compoundaffix2", "iso-8859-1"],
+//  ["compoundaffix3", "iso-8859-1"],
+//  ["compoundflag", "iso-8859-1"],
+//  ["compoundrule", "iso-8859-1"],
+//  ["compoundrule2", "iso-8859-1"],
+//  ["compoundrule3", "iso-8859-1"],
+//  ["compoundrule4", "iso-8859-1"],
+  ["compoundrule5", "UTF-8"],
+//  ["compoundrule6", "iso-8859-1"],
+//  ["compoundrule7", "iso-8859-1"],
+//  ["compoundrule8", "iso-8859-1"],
+  ["condition", "iso-8859-2"],
+  ["condition_utf", "UTF-8"],
+//  ["conditionalprefix", "iso-8859-1"],
+
+  ["digits_in_words", "UTF-8"],
+  ["dotless_i", "UTF-8"],
+
+  ["encoding", "iso-8859-15", { 1: "todo", 3: "todo" }],
+
+//  ["flag", "iso-8859-1"],
+//  ["flaglong", "iso-8859-1"],
+//  ["flagnum", "iso-8859-1"],
+//  ["flagutf8", "UTF-8"],
+//  ["fogemorpheme", "iso-8859-1"],
+//  ["forbiddenword", "iso-8859-1"],
+//  ["forceucase", "iso-8859-1"],
+  ["fullstrip", "iso-8859-15"],
+
+//  ["germancompounding", "iso-8859-1"],
+//  ["germancompoundingold", "iso-8859-1"],
+
+  ["hu", "UTF-8"],
+
   ["i35725", "iso-8859-1"],
-  ["i53643", "iso-8859-1"],
+//  ["i53643", "iso-8859-1"],
   ["i54633", "iso-8859-1"],
-  ["i54980", "iso-8859-1", { 1: "todo", 3: "todo" }],
-  ["maputf", "UTF-8"],
-  ["reputf", "UTF-8"],
-  ["ignore", "iso-8859-1"],
+  ["i54980", "iso-8859-15", { 1: "todo", 3: "todo" }],
+//  ["i58202", "iso-8859-1"],
+//  ["i68568", "iso-8859-1"],
+  ["i68568utf", "UTF-8"],
+  ["iconv", "UTF-8"],
+//  ["iconv2", "UTF-8"],
+//  ["ignore", "iso-8859-1"],
   [
     "ignoreutf",
     "UTF-8",
@@ -96,17 +122,12 @@ const tests = [
       8: "todo",
     },
   ],
-  ["1592880", "iso-8859-1"],
-  ["1695964", "iso-8859-1"],
-  ["1463589", "iso-8859-1"],
-  ["1463589-utf", "UTF-8"],
-  ["IJ", "iso-8859-1"],
-  ["i68568", "iso-8859-1"],
-  ["i68568utf", "UTF-8"],
-  ["1706659", "iso-8859-1"],
-  ["digits-in-words", "iso-8859-1"],
-  //    ["colons-in-words", "iso-8859-1"], Suggestion test only
-  ["ngram-utf-fix", "UTF-8"],
+
+//  ["keepcase", "iso-8859-1"],
+  ["korean", "UTF-8"],
+
+//  ["map", "iso-8859-1"],
+  ["maputf", "UTF-8"],
   [
     "morph",
     "us-ascii",
@@ -130,31 +151,50 @@ const tests = [
       27: "todo",
     },
   ],
-  ["1975530", "UTF-8"],
-  ["fullstrip", "iso-8859-1"],
-  ["iconv", "UTF-8"],
+
+//  ["needaffix", "iso-8859-1"],
+//  ["needaffix2", "iso-8859-1"],
+//  ["needaffix3", "iso-8859-1"],
+//  ["needaffix4", "iso-8859-1"],
+//  ["needaffix5", "iso-8859-1"],
+  ["nepali", "UTF-8"],
+  ["ngram_utf_fix", "UTF-8"],
+//  ["nosuggest", "iso-8859-1"],
+
   ["oconv", "UTF-8"],
-  ["encoding", "iso-8859-1", { 1: "todo", 3: "todo" }],
-  ["korean", "UTF-8"],
-  ["opentaal-forbiddenword1", "UTF-8"],
-  ["opentaal-forbiddenword2", "UTF-8"],
-  ["opentaal-keepcase", "UTF-8"],
-  ["arabic", "UTF-8"],
-  ["2970240", "iso-8859-1"],
-  ["2970242", "iso-8859-1"],
-  ["breakoff", "iso-8859-1"],
-  ["opentaal-cpdpat", "iso-8859-1"],
-  ["opentaal-cpdpat2", "iso-8859-1"],
-  ["2999225", "iso-8859-1"],
-  ["onlyincompound2", "iso-8859-1"],
-  ["forceucase", "iso-8859-1"],
-  ["warn", "iso-8859-1"],
+//  ["onlyincompound", "iso-8859-1"],
+//  ["onlyincompound2", "iso-8859-1"],
+//  ["opentaal-cpdpat", "iso-8859-1"],
+//  ["opentaal-cpdpat2", "iso-8859-1"],
+//  ["opentaal-forbiddenword1", "UTF-8"],
+//  ["opentaal-forbiddenword2", "UTF-8"],
+//  ["opentaal-keepcase", "UTF-8"],
+
+//  ["phone", "iso-8859-1"],
+
+//  ["rep", "iso-8859-1"],
+  ["reputf", "UTF-8"],
+
+//  ["simplifiedtriple", "iso-8859-1"],
+//  ["slash", "iso-8859-1"],
+//  ["sug", "iso-8859-1"],
+  ["sugutf", "UTF-8"],
+
+  ["utf8", "UTF-8"],
+  ["utf8_bom", "UTF-8", { 1: "todo" }],
+  ["utf8_bom2", "UTF-8", { 1: "todo" }],
+  ["utf8_nonbmp", "UTF-8", { 1: "todo", 2: "todo", 3: "todo", 4: "todo" }],
+  ["utfcompound", "UTF-8"],
+
+//  ["warn", "iso-8859-1"],
+
+//  ["zeroaffix", "iso-8859-1"],
 ];
 
 // eslint-disable-next-line no-shadow
 function* do_get_file_by_line(file, charset) {
-  dump("getting file by line for file " + file.path + "\n");
-  dump("using charset1" + charset + "\n");
+  dump("Getting file by line for file " + file.path + "\n");
+  dump("Using charset " + charset + "\n");
   let fis = Cc["@mozilla.org/network/file-input-stream;1"].createInstance(
     Ci.nsIFileInputStream
   );
@@ -175,9 +215,9 @@ function* do_get_file_by_line(file, charset) {
 
 function do_run_test(checker, name, charset, todo_good, todo_bad) {
   dump("\n\n\n\n");
-  dump("running test for " + name + "\n");
+  dump("Running test for " + name + "\n");
   if (!checker) {
-    do_throw("Need spell checker here!");
+    do_throw("Need a spelling checker here");
   }
 
   let good = do_get_file("data/" + name + ".good", true);
@@ -236,10 +276,10 @@ function run_test() {
     Ci.mozISpellCheckingEngine
   );
 
-  Assert.ok(!!spellChecker, "Should have a spell checker");
+  Assert.ok(!!spellChecker, "Should have a spelling checker");
   spellChecker.QueryInterface(Ci.mozISpellCheckingEngine);
   let testdir = do_get_file("data/", false);
-  spellChecker.loadDictionariesFromDir(testdir);
+  spellChecker.loadDictionariesFromDir(testdir); //TODO Error occurs here.
 
   function do_run_test_closure(test) {
     let [name, charset, todo_good, todo_bad] = test;
--- extensions/spellcheck/nuspell/tests/unit/test_nuspell_unicode_paths.js
+++ extensions/spellcheck/nuspell/tests/unit/test_nuspell_unicode_paths.js
@@ -1,3 +1,21 @@
+/* Copyright 2020 Sander van Geloven, Dimitrij Mijoski
+ *
+ * This file is part of Nuspell.
+ *
+ * Nuspell is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nuspell is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Nuspell.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 "use strict";
 
 const { XPCOMUtils } = ChromeUtils.import(
@@ -21,8 +39,10 @@ const nsFile = Components.Constructor(
 add_task(async function() {
   let prof = do_get_profile();
 
+  // Using https://codepoints.net/U+263A as UTF-8 character.
   let basePath = OS.Path.join(prof.path, "\u263a", "dictionaries");
   let baseDir = nsFile(basePath);
+  // This will create the directory /tmp/firefox/xpcshellprofile/☺/ on POSIX.
   await OS.File.makeDir(basePath, { from: prof.path });
 
   let dicPath = OS.Path.join(basePath, "dict.dic");
@@ -33,7 +53,7 @@ add_task(async function() {
   await OS.File.writeAtomic(dicPath, new TextEncoder().encode(`1\n${WORD}\n`));
   await OS.File.writeAtomic(affPath, new TextEncoder().encode(""));
 
-  spellCheck.loadDictionariesFromDir(baseDir);
+  spellCheck.loadDictionariesFromDir(baseDir); //TODO Error occurs here.
   spellCheck.dictionary = "dict";
 
   ok(
