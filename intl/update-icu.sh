35850772cfcf8207315e6624dfd242a454efe445
PanderMusubi <pander@users.sourceforge.net>
2020-06-12 18:18:21 +0200

    skip hg addremove for icu config change

--- intl/update-icu.sh
+++ intl/update-icu.sh
@@ -66,7 +66,7 @@ python ${topsrcdir}/js/src/tests/non262/String/make-normalize-generateddata-inpu
 # build a new ICU data file.
 python `dirname $0`/icu_sources_data.py $topsrcdir
 
-hg addremove "${icu_dir}/source" "${icu_dir}/GIT-INFO" ${topsrcdir}/config/external/icu
+#hg addremove "${icu_dir}/source" "${icu_dir}/GIT-INFO" ${topsrcdir}/config/external/icu
 
 # Check local tzdata version.
 `dirname $0`/update-tzdata.sh -c
